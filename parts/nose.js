59b532992540d925aeef270bb826c56e33ff7374
Beau Gunderson <beau@beaugunderson.com>
2016-02-02 11:25:09 -0800

    add triangle nose

--- parts/nose.js
+++ parts/nose.js
@@ -1,20 +1,46 @@
 'use strict';
 
+var bezierTo = require('../lib/bezier-to.js');
 var _ = require('lodash');
+
+function roundNose(ctx, options) {
+  ctx.ellipse(0, options.noseOffsetY,
+              options.headWidth * _.random(0.05, 0.1),
+              options.headHeight * _.random(0.025, 0.065),
+              0, 0, 2 * Math.PI);
+}
+
+function triangleNose(ctx, options) {
+  var noseWidth = options.headWidth * _.random(0.1, 0.2);
+  var noseHeight = options.headHeight * _.random(0.1, 0.2);
+
+  function side(offset) {
+    bezierTo(
+      ctx,
+       0, options.noseOffsetY - noseHeight,
+       offset * noseWidth, options.noseOffsetY - noseHeight,
+       offset * noseWidth * 0.9, options.noseOffsetY - (noseHeight * 0.75),
+       0, options.noseOffsetY);
+  }
+
+  side(-1);
+  side(1);
+}
 
 module.exports = function (ctx, options) {
   ctx.save();
 
   ctx.translate(options.centerX, options.centerY);
 
-  ctx.fillStyle = 'black';
+  ctx.fillStyle = options.noseColor;
+
+  if (_.random(0, 100) >= 50) {
+    ctx.strokeStyle = options.noseColor;
+  }
 
   ctx.beginPath();
 
-  ctx.ellipse(0, options.noseOffsetY,
-              options.headWidth * _.random(0.05, 0.1),
-              options.headHeight * _.random(0.025, 0.065),
-              0, 0, 2 * Math.PI);
+  _.sample([roundNose, triangleNose])(ctx, options);
 
   ctx.fill();
   ctx.stroke();
