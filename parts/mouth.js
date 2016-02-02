a3b09af8f64e3dea11aacbd52e6fea8aa41ff99b
Beau Gunderson <beau@beaugunderson.com>
2016-02-02 11:24:39 -0800

    add drop mouth

--- parts/mouth.js
+++ parts/mouth.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var bezierTo = require('../lib/bezier-to.js');
+var _ = require('lodash');
 
 module.exports = function (ctx, options) {
   ctx.save();
@@ -9,21 +10,28 @@ module.exports = function (ctx, options) {
 
   ctx.beginPath();
 
-  // left side
-  bezierTo(
-    ctx,
-    0, options.noseOffsetY,
-    -options.mouthWidth * 0.25, options.noseOffsetY + options.mouthHeight,
-    -options.mouthWidth * 0.75, options.noseOffsetY + options.mouthHeight,
-    -options.mouthWidth, options.noseOffsetY * 1.05);
+  var dropMouthOffset = 0;
 
-  // right side
-  bezierTo(
-    ctx,
-    0, options.noseOffsetY,
-    options.mouthWidth * 0.25, options.noseOffsetY + options.mouthHeight,
-    options.mouthWidth * 0.75, options.noseOffsetY + options.mouthHeight,
-    options.mouthWidth, options.noseOffsetY * 1.05);
+  if (_.random(0, 100) >= 50) {
+    dropMouthOffset = options.headHeight * _.random(0.125, 0.15);
+
+    ctx.moveTo(0, options.noseOffsetY);
+    ctx.lineTo(0, options.noseOffsetY + dropMouthOffset);
+  }
+
+  var startY = options.noseOffsetY + dropMouthOffset;
+
+  function side(offset) {
+    bezierTo(
+      ctx,
+      0, startY,
+      offset * options.mouthWidth * 0.25, startY + options.mouthHeight,
+      offset * options.mouthWidth * 0.75, startY + options.mouthHeight,
+      offset * options.mouthWidth, startY * 1.05);
+  }
+
+  side(-1);
+  side(1);
 
   ctx.stroke();
 
