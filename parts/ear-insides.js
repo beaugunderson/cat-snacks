84f1131a8e12f81a4251c67c5cef5cd099455453
Beau Gunderson <beau@beaugunderson.com>
2016-02-02 22:17:48 -0800

    refactor ears, add pointy ears

--- parts/ear-insides.js
+++ parts/ear-insides.js
@@ -4,35 +4,65 @@ var bezierTo = require('../lib/bezier-to.js');
 var Color = require('color');
 var _ = require('lodash');
 
-function earInside(ctx, options, flip) {
-  var offset = flip ? -1 : 1;
+function pointyInsides(ctx, options) {
+  function side(offset) {
+    ctx.beginPath();
 
-  ctx.beginPath();
+    ctx.moveTo(options.headWidth * 0.725 * offset,
+               -options.headHeight * 0.55);
 
-  bezierTo(
-    ctx,
-    -options.headWidth * 0.725 * offset,
-    -options.headHeight * 0.55,
+    ctx.lineTo(options.headWidth * (0.635 * options.earFactorX) * offset,
+               -options.headHeight * (1.05 * options.earFactorY));
 
-    // control point 1
-    -options.headWidth * (0.725 * options.earFactorX) * offset,
-    -options.headHeight * (1.2 * options.earFactorY),
+    ctx.lineTo(options.headWidth * 0.45 * offset,
+               -options.headHeight * 0.75);
 
-    // control point 2
-    -options.headWidth * (0.7 * options.earFactorX) * offset,
-    -options.headHeight * (1.2 * options.earFactorY),
+    ctx.fill();
+  }
 
-    -options.headWidth * 0.45 * offset,
-    -options.headHeight * 0.75);
-
-  ctx.fill();
+  side(-1);
+  side(1);
 }
 
-module.exports = function earInsides(ctx, options) {
+function curvedInsides(ctx, options) {
+  function side(offset) {
+    ctx.beginPath();
+
+    bezierTo(
+      ctx,
+      options.headWidth * 0.725 * offset,
+      -options.headHeight * 0.55,
+
+      // control point 1
+      options.headWidth * (0.725 * options.earFactorX) * offset,
+      -options.headHeight * (1.2 * options.earFactorY),
+
+      // control point 2
+      options.headWidth * (0.7 * options.earFactorX) * offset,
+      -options.headHeight * (1.2 * options.earFactorY),
+
+      options.headWidth * 0.45 * offset,
+      -options.headHeight * 0.75);
+
+    ctx.fill();
+  }
+
+  side(-1);
+  side(1);
+}
+
+module.exports = function (ctx, options) {
+  ctx.save();
+
   ctx.fillStyle = Color(options.catColor)
     .lighten(_.sample([-0.075, 0.075]))
     .hexString();
 
-  earInside(ctx, options, false);
-  earInside(ctx, options, true);
+  if (options.usedFunctions.ears.insides === 'pointy') {
+    pointyInsides(ctx, options);
+  } else if (options.usedFunctions.ears.insides === 'curved') {
+    curvedInsides(ctx, options);
+  }
+
+  ctx.restore();
 };
--- parts/ears.js
+++ parts/ears.js
@@ -1,34 +1,64 @@
 'use strict';
 
 var bezierTo = require('../lib/bezier-to.js');
+var _ = require('lodash');
 
-function ear(ctx, options, flip) {
-  var offset = flip ? -1 : 1;
+function pointyEars(ctx, options) {
+  options.usedFunctions.ears.insides = 'pointy';
 
+  function side(offset) {
+    ctx.moveTo(options.headWidth * 0.8 * offset,
+               -options.headHeight * 0.5);
+
+    ctx.lineTo(options.headWidth * (0.65 * options.earFactorX) * offset,
+               -options.headHeight * (1.15 * options.earFactorY));
+
+    ctx.lineTo(options.headWidth * 0.4 * offset,
+               -options.headHeight * 0.8);
+  }
+
+  side(-1);
+  side(1);
+}
+
+function curvedEars(ctx, options) {
+  options.usedFunctions.ears.insides = 'curved';
+
+  function side(offset) {
+    bezierTo(
+      ctx,
+      options.headWidth * 0.8 * offset,
+      -options.headHeight * 0.5,
+
+      // control point 1
+      options.headWidth * (0.8 * options.earFactorX) * offset,
+      -options.headHeight * (1.35 * options.earFactorY),
+
+      // control point 2
+      options.headWidth * (0.65 * options.earFactorX) * offset,
+      -options.headHeight * (1.35 * options.earFactorY),
+
+      options.headWidth * 0.4 * offset,
+      -options.headHeight * 0.8);
+  }
+
+  side(-1);
+  side(1);
+}
+
+module.exports = function (ctx, options) {
   ctx.save();
 
   ctx.translate(options.centerX, options.centerY);
 
-  bezierTo(
-    ctx,
-    -options.headWidth * 0.8 * offset,
-    -options.headHeight * 0.5,
+  if (!options.usedFunctions.ears) {
+    options.usedFunctions.ears = _.sample([
+      curvedEars,
+      pointyEars
+    ]);
+  }
 
-    // control point 1
-    -options.headWidth * (0.8 * options.earFactorX) * offset,
-    -options.headHeight * (1.35 * options.earFactorY),
-
-    // control point 2
-    -options.headWidth * (0.65 * options.earFactorX) * offset,
-    -options.headHeight * (1.35 * options.earFactorY),
-
-    -options.headWidth * 0.4 * offset,
-    -options.headHeight * 0.8);
+  options.usedFunctions.ears(ctx, options);
 
   ctx.restore();
-}
-
-module.exports = function (ctx, options) {
-  ear(ctx, options, false);
-  ear(ctx, options, true);
 };
