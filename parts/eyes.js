7e37ff483145d582bbdf930d6057e26cb76b0eba
Beau Gunderson <beau@beaugunderson.com>
2016-02-10 23:03:16 -0800

    add destructuring assignment for options

--- parts/eyes.js
+++ parts/eyes.js
@@ -2,21 +2,25 @@
 
 var _ = require('lodash');
 
-module.exports = function (ctx, options) {
+module.exports = function (ctx) {
+  var {accessories, centerX, centerY, closedEyes, eyeColor, eyeOffsetY,
+    eyeSize, headWidth, oddEyeColor, oddEyePercentage} =
+      require('../lib/config.js');
+
   ctx.save();
 
-  ctx.translate(options.centerX, options.centerY);
+  ctx.translate(centerX, centerY);
 
-  if (_.random(0, 100) <= options.oddEyePercentage) {
-    ctx.fillStyle = options.oddEyeColor;
+  if (_.random(0, 100) <= oddEyePercentage) {
+    ctx.fillStyle = oddEyeColor;
   } else {
-    ctx.fillStyle = options.eyeColor;
+    ctx.fillStyle = eyeColor;
   }
 
   function side(offset) {
     ctx.beginPath();
 
-    if (options.closedEyes) {
+    if (closedEyes) {
       closedEye(offset);
     } else {
       openEye(offset);
@@ -24,16 +28,16 @@ module.exports = function (ctx, options) {
   }
 
   function openEye(offset) {
-    ctx.ellipse(offset * options.headWidth * 0.4,
-                options.eyeOffsetY,
-                options.eyeSize,
-                options.eyeSize,
+    ctx.ellipse(offset * headWidth * 0.4,
+                eyeOffsetY,
+                eyeSize,
+                eyeSize,
                 0, 0, 2 * Math.PI);
   }
 
   function closedEye(offset) {
-    var eyeX = offset * options.headWidth * 0.5;
-    var eyeWidth = options.eyeSize * 3;
+    var eyeX = offset * headWidth * 0.5;
+    var eyeWidth = eyeSize * 3;
 
     if (offset == 1) {
       eyeX -= eyeWidth;
@@ -42,16 +46,16 @@ module.exports = function (ctx, options) {
     ctx.fillStyle = 'black';
 
     ctx.rect(eyeX,
-             options.eyeOffsetY,
+             eyeOffsetY,
              eyeWidth,
-             options.eyeSize / 2);
+             eyeSize / 2);
   }
 
   side(-1);
 
   ctx.fill();
 
-  ctx.fillStyle = options.eyeColor;
+  ctx.fillStyle = eyeColor;
 
   side(1);
 
