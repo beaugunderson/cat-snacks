da5b1eef510861e62353d998417cb0ef0f5a1a5a
Beau Gunderson <beau@beaugunderson.com>
2016-02-10 23:27:52 -0800

    fix grid script

--- test/test-grid.js
+++ test/test-grid.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var catGrid = require('../lib/generate-grid.js');
 var fs = require('fs');
+var grid = require('../lib/generate-grid.js');
 
 console.log('generating...');
 
-catGrid(2048, 5, function (err, buffer) {
+grid(2048, 5, {}, function (err, buffer) {
   console.log('writing...');
 
   fs.writeFileSync('./output-grid.png', buffer);
